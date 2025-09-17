c2740bd7661e783062b004c041ef093e1d95d4d7
Богдан Корогодский <bogdankorogodskij@gmail.com>
2025-09-17 22:03:29 +0300

    ауфыв

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Layout } from './common-ui/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Layout}
+];
